9b4d922a7efdc674259a795d83c97d80d3bfc145
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-10 15:54:19 -0300

    Cria propriedade ":name" na url /users. Cria passagem de props no Route/Component Users. Renderiza User dinamicamente utilizando propriedade "name" da url

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,10 +4,11 @@ import { Link } from 'react-router-dom';
 class Users extends Component {
   render() {
     const { greetingsMessage } = this.props
+    const {match: {params: {name}}} = this.props;
     return (
       <div>
         <h2>Users</h2>
-        <p> { greetingsMessage }, My awesome Users component </p>
+        <p> { greetingsMessage } {name.toUpperCase()}, My awesome Users component </p>
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
